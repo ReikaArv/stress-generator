4ae39b220b3c740423f4a6ade88f8d1af7599e5d
Efga Chandra <67002422+ReikaArv@users.noreply.github.com>
2021-01-17 00:26:17 +0700

    Add files via upload
    a

---
+++ Stress/script.js
@@ -0,0 +1,47 @@
+function ceplokceplok(){
+    var nama = document.getElementById('nama_input').value;
+    var output = document.getElementById('output-text');
+
+    var ceplok = 'BUKA BAJU name LANGSUNG CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROT CROT CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROT MONNCROOOTOOOOOOOOOOOOOOOOOOOOOOOTTTTTTTTTTTTTT'
+
+    var replace1 = ceplok.replace('name', nama)
+    output.innerHTML = replace1;
+
+}
+
+function wangiwangi(){
+    var nama = document.getElementById('nama_input').value;
+    var output = document.getElementById('output-text');
+
+    var wangi = 'name name name ❤️ ❤️ ❤️ WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya rambut name wangi aku mau nyiumin aroma wanginya name AAAAAAAAH ~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~~~~ AAAAAH name keluar pertama kali di anime juga manis ❤️ ❤️ ❤️ banget AAAAAAAAH name AAAAA LUCCUUUUUUUUUUUUUUU............name AAAAAAAAAAAAAAAAAAAAGH ❤️ ❤️ ❤️apa ? name itu gak nyata ? Cuma HALU katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK PEDULI BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. ❤️ ❤️ ❤️ name gw ...name di laptop ngeliatin gw, name .. kamu percaya sama aku ? aaaaaaaaaaah syukur name aku gak mau merelakan name aaaaaah ❤️ ❤️ ❤️ YEAAAAAAAAAAAH GUA MASIH PUNYA name SENDIRI PUN NGGAK SAMA AAAAAAAAAAAAAA'
+
+    
+    var replace2 = wangi.replace(/name/g, nama);
+        
+    output.innerHTML = replace2;
+}
+
+function halusinasi(){
+    var nama = document.getElementById('nama_input').value;
+    var output = document.getElementById('output-text');
+
+    var halu = 'Buruan, panggil gue SIMP, ato BAPERAN. ini MURNI PERASAAN GUE. Gue pengen genjot bareng name. Ini seriusan, suaranya yang imut, mukanya yang cantik, apalagi badannya yang aduhai ningkatin gairah gue buat genjot name. Setiap lapisan kulitnya pengen gue jilat. Saat gue mau crot, gue bakal moncrot sepenuh hati, bisa di perut, muka, badan, teteknya, sampai lubang burit pun bakal gue crot sampai puncak klimaks. Gue bakal meluk dia abis gue moncrot, lalu nanya gimana kabarnya, ngrasain enggas bareng saat telanjang. Dia bakal bilang kalau genjotan gue mantep dan nyatain perasaannya ke gue, bilang kalo dia cinta ama gue. Gue bakal bilang balik seberapa gue cinta ama dia, dan dia bakal kecup gue di pipi. Terus kita ganti pakaian dan ngabisin waktu nonton film, sambil pelukan ama makan hidangan favorit. Gue mau name jadi pacar, pasangan, istri, dan idup gue. Gue cinta dia dan ingin dia jadi bagian tubuh gue. Lo kira ini copypasta? Kagak cok. Gue ngetik tiap kata nyatain prasaan gue. Setiap kali elo nanya dia siapa, denger ini baik-baik : DIA ISTRI GUE. Gue sayang name, dan INI MURNI PIKIRAN DAN PERASAAN GUE'
+
+    var replace3 = halu.replace('name', nama)
+    output.innerHTML = replace3;
+}
+//class codinganstress {
+//    constructor (nama) {
+//        this.nama = null
+//    }
+//   stress_() {
+//        function stress(){
+//            var nama = document.getElementById('nama_input').value;
+//            var output = document.getElementById('output-text');
+//        
+//            var ceplok = 'BUKA BAJU name LANGSUNG CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK //CEPLOK CEPLOK CROT CROT CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROT MONNCROOOTOOOOOOOOOOOOOOOOOOOOOOOTTTTTTTTTTTTTT'
+        
+//            var replace1 = ceplok.replace('name', nama)
+//            output.innerHTML = replace1;
+//    }
+//}
